7a16d966a1eb720a25441d17af50947657b7ee39
ekgns <ekgns14@naver.com>
2021-08-26 17:45:49 +0900

    구현한 메서드 기능 주석으로 정리 함

--- NMap_Practice/NaverMap_Pracite/NaverMap_Pracite/ViewController/ViewController.swift
+++ NMap_Practice/NaverMap_Pracite/NaverMap_Pracite/ViewController/ViewController.swift
@@ -53,6 +53,7 @@ class ViewController: UIViewController  {
 
     }
     
+    // MARK: - 내 위치로 카메라 이동
     func moveCamera() {
         currentLocation()
         let cameraUpdate = NMFCameraUpdate(scrollTo:  NMGLatLng(lat: lat ?? 0.0, lng: lng ?? 0.0), zoomTo: 17)
@@ -62,19 +63,12 @@ class ViewController: UIViewController  {
     
     
     func getCenterList() {
-        viewModel.requestCenterList(page: 1, perPage: 284) { data in
-            self.centers = data.data
-            for (index, number) in self.centers.enumerated() {
-                
-                let infoWindow = NMFInfoWindow()
-                let dataSource = NMFInfoWindowDefaultTextSource.data()
-                dataSource.title = "\(number.centerName)"
-                infoWindow.dataSource = dataSource
-                
-//                let marker = NMFMarker()
-                infoWindow.position = NMGLatLng(lat:(Double(number.lat) ?? 0.0), lng:(Double(number.lng) ?? 0.0))
-                infoWindow.mapView = self.mapView
-                
+        viewModel.requestCenterList(page: 1, perPage: 284) { data in // 뷰 모델에 있는 리퀘스트 쏀터 리스트 사용 성공시 넘어오는 데이터 data 안에 넣어줌
+            // 넘어온 data는 CenterListVO타입의 배열
+            self.centers = data.data // 전역에 선언한 Center 배열 타입의 centers에 data에 넣어서 넘어온 CenterListVO안의 Center 배열을 넣어준다
+            for (_, center) in self.centers.enumerated() { // 인덱스 번호만큼 반복/  Centers의 값에서 원하는값을 뽑아 사용 한다
+                // 센터이름으로 마커 찍는 메서드를 실행 넘어온 data의 수 만큼 마커를 찍는다 센터의 이름과 위도, 경도를 넣는다
+                self.centerMarkers(title:center.centerName, lat: Double(center.lat) ?? 0.0, lng: Double(center.lng) ?? 0.0)
                     }
                 
         } failure: { error in
@@ -85,10 +79,27 @@ class ViewController: UIViewController  {
         }
     }
     
-   
-    
+// MARK: - 센터이름으로 마커 찍기 (마커에 필요한 정보는 이름, 위도,경도)
+    func centerMarkers(title: String, lat: Double, lng: Double) {
+        // 정보창
+        let infoWindow = NMFInfoWindow()
+        // 정보창에 데이터
+        let dataSource = NMFInfoWindowDefaultTextSource.data()
+        // 센터 이름
+        dataSource.title = title
+        
+        // 정보창 마커에 데이터를 데이터 소스를 선언
+        infoWindow.dataSource = dataSource
+        
+        // 정보창 위치 표시
+        infoWindow.position = NMGLatLng(lat: lat , lng: lng)
+        
+        // 센터 위치 맵에 표시
+        infoWindow.mapView = self.mapView
+    }
     
     func currentLoactionOverray() {
+        // 내위치 표시
         mapView.positionMode = .direction
     }
 }
